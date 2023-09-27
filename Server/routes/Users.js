56c34e99f4c77c144ff63a00fb914107dc5b8ad8
vurados <kryuchkov2001@mail.ru>
2023-09-27 18:53:53 +0300

    duoble prev commit

--- Server/routes/Users.js
+++ Server/routes/Users.js
@@ -14,7 +14,7 @@ router.post('/', async(req, res) => {
         res.status(200).json({msg:'User successfully signed up', data: newData})
     }catch(error){
         console.error(error)
-        res.json({status:500, msg: 'Error creating user entity', error: error.message})
+        res.json({msg: 'Error creating user entity', error: error.message})
     }
 
     // res.send('The error ocurred during creating(posting) user entity into the table')
