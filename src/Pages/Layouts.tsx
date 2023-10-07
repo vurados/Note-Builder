3bc1ed45fca027459ed2a447dc48b8510a496d4d
vurados <kryuchkov2001@mail.ru>
2023-10-08 00:03:36 +0300

    commit minor changes

--- src/Pages/Layouts.tsx
+++ src/Pages/Layouts.tsx
@@ -22,22 +22,7 @@ export function Layouts(){
 
     useEffect(() => {
       fetchLayouts()
-
-
-        // if(user.isAuthentificated){
-        //   axios.post("http://localhost:3001/layouts/getLayouts", user).then((res) =>{
-        //     // console.log(res.data);
-        //     setListOfLayouts(res.data)
-        //     console.log('set list of layouts', listOfLayouts);
-        //     // const lastElement = listOfLayouts.slice(-1)
-        //     // console.log('last element', lastElement);
-            
-        //     // const iddd = lastElement[0]['id']
-        //     // console.log('id', iddd);
-        //     // listOfLayouts.slice(-1)[0]['id']
-        //   })
-        // }
-    }, [])
+    })
 
     const fetchLayouts = async () => {
       if(user.isAuthentificated){
@@ -47,13 +32,17 @@ export function Layouts(){
           })
         }
     }
-    // TODO:
-    // i need function {onDeleteHandler} that will not request server data through fetchLayouts and just delete the entry using setListLayouts
-
+    
     const createHandler = (layout:any) => {
       setListOfLayouts(prev=> {
         return [...prev, layout]
       })
+    }
+
+    // TODO:
+    // i need function {onDeleteHandler} that will not request server data through fetchLayouts and just delete the entry using setListLayouts
+    const deleteHandler = () => {
+      fetchLayouts()
     }
 
     // add onSubmit function in layoutmodal component as onClose that will call function in here that will setListOflayouts
@@ -62,7 +51,7 @@ export function Layouts(){
       <div className='flex flex-col gap-10 min-h-70'>
         <TopBar />
         <div id="main" className='container grid grid-cols-4 gap-x-3 gap-y-3 mx-auto text-center min-h-screen pb-96'>
-          {listOfLayouts.map((layout: ILayouts) => <LayoutTile onDelete={fetchLayouts} layout={layout} key={layout.id} />)}
+          {listOfLayouts.map((layout: ILayouts) => <LayoutTile onDelete={deleteHandler} layout={layout} key={layout.id} />)}
           <div onClick={() => setModal(true)}><AddNoteTile /></div>
         </div>
         <Footer />
